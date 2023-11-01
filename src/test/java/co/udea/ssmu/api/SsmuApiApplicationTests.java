9136ab50f199f65923179afdf9006e9a81b7ac1e
Felipe Correa <fdejesus.correa@udea.edu.co>
2023-11-01 16:55:21 -0500

    another commit

--- src/test/java/co/udea/ssmu/api/SsmuApiApplicationTests.java
+++ src/test/java/co/udea/ssmu/api/SsmuApiApplicationTests.java
@@ -6,4 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SsmuApiApplicationTests {
     private int ome = 0;
+
+    private int ome = 0;
+    private int ome = 0;
+    private int ome = 0;
+    private int ome = 0;
 }
